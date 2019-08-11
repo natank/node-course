e5d9aa41565610615cc2789827f27ade153057aa
Nati <natan.kamusher@gmail.com>
2019-08-11 21:43:29 +0300

    Saved for remote

--- app.js
+++ app.js
@@ -3,36 +3,19 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const { headers, method, url } = req;
-
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    let body = [];
-    req
-      .on('data', chunk => {
-        body.push(chunk);
-      })
-      .on('end', () => {
-        body = Buffer.concat(body)
-          .toString()
-          .split('=')[1];
-        fs.writeFileSync('message.text', body);
-      });
-
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+  } else if (url === '/message' && method === 'POST') {
+    console.log('message route');
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Greeting from the server</h1></body>');
   res.write('</html>');
   res.end();
 });
