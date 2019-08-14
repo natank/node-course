5332497303578795c8755157dc61222d9183b88c
Nati <natan.kamusher@gmail.com>
2019-08-14 16:25:14 +0300

    Completed part one of the section

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-let http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next)=>{
+    console.log("This always runs!");
+    next();
+})
+
+app.use('/add-product', (req, res, next)=>{
+    console.log('In another middleware');
+    res.send('<h1>The "Add Product" Page</h1>')
+})
+
+app.use('/',(req, res, next)=>{
+    console.log('<h1>In another middleware</h1>');
+    res.send('<h1>hello from express</h1>')
+})
+
 
 let processRequest = require('./processRequest');
 
-http.createServer(processRequest).listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server is running at http://127.0.0.1:3000/')
 });
