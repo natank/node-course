634c7e614a48c23ee8307a67a196fce9437c73dd
Nati <natan.kamusher@gmail.com>
2019-08-15 18:50:32 +0300

    Completed configuring the static dir

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const rootFolder = require('./utils/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const r404Routes = require('./routes/404');
+
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
+app.use(express.static(path.join(rootFolder, 'public')));
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 app.use(r404Routes);
