703db1808e80689a63e446cd46ef623a8be1a28f
Nati <natan.kamusher@gmail.com>
2019-08-16 08:38:38 +0300

    Copied starter files

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static('public'));
 
-const user = require('./routes/user');
-const show = require('./routes/show');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.get('/', (req, res) => {
-  res.redirect('/show')
-})
-app.use('/user', user)
-app.use('/show', show)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => console.log("server listening on port 3000"))
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
--- utils/rootFolder.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-let rootFolder = path.join(path.dirname(process.mainModule.filename))
-
-module.exports = rootFolder;
