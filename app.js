e59771c346ce9deeea1239cfc1cb093b51944b8f
Nati <natan.kamusher@gmail.com>
2019-08-19 21:38:03 +0300

    Completed assignment 4

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = require('./scripts/router');
-// const router = express.Router();
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set('view engine', 'pug');
 app.set('views', 'views');
 app.use(express.static('public'))
 app.use('/', router);
 
 
-app.listen(3000,()=> console.log('app is listening on port 3000'))
+app.listen(3000, () => console.log('app is listening on port 3000'))
--- scripts/router.js
+++ scripts/router.js
@@ -4,16 +4,28 @@ const router = express.Router();
 
 const users = ["Ami", "Haya"];
 
-router.get('/users', (req, res)=>{
-    res.render('show-users', {users})
+
+
+router.get('/users', (req, res) => {
+    res.render('show-users', {
+        users,
+        title: "users"
+    })
 })
 
-router.get('/add-user', (req, res)=>{
-    res.render('add-user', {title: "Add User"})
+router.get('/add-user', (req, res) => {
+    res.render('add-user', {
+        title: "Add User"
+    })
 })
 
-router.post('/user', (req, res)=>{
-    
+router.post('/user', (req, res) => {
+    users.push(req.body.username);
+    res.redirect('/add-user');
+})
+
+router.get('/', (req, res) => {
+    res.redirect('/add-user')
 })
 
 module.exports = router;
