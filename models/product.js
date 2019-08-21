cd32d68955c69877b1160ceee0aa74060fb322dd
Nati <natan.kamusher@gmail.com>
2019-08-21 10:38:07 +0300

    completed async saving and loading from file

--- models/product.js
+++ models/product.js
@@ -6,29 +6,32 @@ module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
-    save() {
+    async save() {
+        let products = [];
+        try{
+            products = await readProductsFromFile();
+        } catch(err){
+            console.log(err);
+        }
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+            console.log(err);
+        });
+    }
+    static fetchAll() {
+        return readProductsFromFile();
+    }
+}
 
+function readProductsFromFile(){
+    let pr = new Promise((resolve, reject) => {
+        var products = [];
         fs.readFile(p, (err, fileContent) => {
-            let products = [];
             if (!err) {
                 products = JSON.parse(fileContent);
             }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
-            });
+            resolve(products)
         })
-    }
-    static fetchAll() {
-        let pr = new Promise((resolve, reject) => {
-            var products = [];
-            fs.readFile(p, (err, fileContent) => {
-                if (!err) {
-                    products = JSON.parse(fileContent);
-                }
-                resolve(products)
-            })
-        })
-        return pr;
-    }
+    })
+    return pr;
 }
