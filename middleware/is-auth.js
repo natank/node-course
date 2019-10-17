9a01b9c9f28f22d077547cb35574fe29d8d1cd46
Nati <natan.kamusher@gmail.com>
2019-10-17 23:50:31 +0300

    Initial setup - reset folders files

--- middleware/is-auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if ((req.session.isLoggedIn)) {
-    next();
-  } else {
-    req.flash('error', 'You must be logged in to access this page');
-    return (res.redirect('/login'))
-  }
-}
